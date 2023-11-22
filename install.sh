7a800b91987439e77c32be9e8721d0f5085a7bf5
william chu <william.chu@uptickhq.com>
2023-11-22 16:14:32 +1100

    chore: update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,32 @@
 #! /bin/bash
 # Our public boootstrap entry point
-set -eu
+set -e
 
-echo 1
+cat << "EOF"
+ __        __   _                            _____          ____
+ \ \      / /__| | ___ ___  _ __ ___   ___  |_   _|__      / / /
+  \ \ /\ / / _ \ |/ __/ _ \|  _   _ \ / _ \   | |/ _ \    / / /
+   \ V  V /  __/ | (_| (_) | | | | | |  __/   | | (_) |  / / /
+    \_/\_/ \___|_|\___\___/|_| |_| |_|\___|   |_|\___/  / / /
+          _   _ ____ _____ ___ ____ _      ____        / / /
+         | | | |  _ \_   _|_ _/ ___| | __  \ \ \      / / /
+         | | | | |_) || |  | | |   | |/ /   \ \ \    / / /
+         | |_| |  __/ | |  | | |___|   <     \ \ \  / / /
+          \___/|_|    |_| |___\____|_|\_\     \ \ \/ / /
+                                               \ \  / /
+                                                \ \/ /
+                                                 \__/
+
+EOF
+
+sleep 1
+
+echo "Fetching our bootstrap repository ..."
+
+curl -L -o /tmp/master.zip http://github.com/uptick/bootstrap-public/zipball/master/
+
+unzip /tmp/master.zip -d /tmp/bootstrap
+
+cd /tmp/bootstrap/*/
+ls
+
